fcf5357cce9c002ea0d1acf4cc2d3b4265c99b20
kimkihwan <kihwan0101@gmail.com>
2016-05-24 21:39:49 +0900

    Database project3

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PSTR = new System.Windows.Forms.Label();
+            this.PNUM = new System.Windows.Forms.NumericUpDown();
+            this.QUERYBTN = new System.Windows.Forms.Button();
+            this.SQLTEXT = new System.Windows.Forms.RichTextBox();
+            this.RESULTTEXT = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PNUM)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PSTR
+            // 
+            this.PSTR.BackColor = System.Drawing.Color.Lime;
+            this.PSTR.Font = new System.Drawing.Font("Consolas", 14F);
+            this.PSTR.Location = new System.Drawing.Point(13, 13);
+            this.PSTR.Name = "PSTR";
+            this.PSTR.Size = new System.Drawing.Size(726, 126);
+            this.PSTR.TabIndex = 0;
+            this.PSTR.Text = "다음 ER 다이어그램을 Oracle DBMS에 입력하시오 (create)";
+            this.PSTR.Click += new System.EventHandler(this.PSTR_Click);
+            // 
+            // PNUM
+            // 
+            this.PNUM.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNUM.Location = new System.Drawing.Point(759, 65);
+            this.PNUM.Maximum = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            this.PNUM.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PNUM.Name = "PNUM";
+            this.PNUM.Size = new System.Drawing.Size(120, 30);
+            this.PNUM.TabIndex = 1;
+            this.PNUM.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PNUM.ValueChanged += new System.EventHandler(this.PNUM_ValueChanged);
+            // 
+            // QUERYBTN
+            // 
+            this.QUERYBTN.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QUERYBTN.Location = new System.Drawing.Point(899, 41);
+            this.QUERYBTN.Name = "QUERYBTN";
+            this.QUERYBTN.Size = new System.Drawing.Size(119, 74);
+            this.QUERYBTN.TabIndex = 2;
+            this.QUERYBTN.Text = "GO";
+            this.QUERYBTN.UseVisualStyleBackColor = true;
+            this.QUERYBTN.Click += new System.EventHandler(this.QUERYBTN_Click);
+            // 
+            // SQLTEXT
+            // 
+            this.SQLTEXT.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SQLTEXT.Location = new System.Drawing.Point(13, 153);
+            this.SQLTEXT.Name = "SQLTEXT";
+            this.SQLTEXT.Size = new System.Drawing.Size(1005, 148);
+            this.SQLTEXT.TabIndex = 3;
+            this.SQLTEXT.Text = "SQLTEXT";
+            this.SQLTEXT.TextChanged += new System.EventHandler(this.SQLTEXT_TextChanged);
+            // 
+            // RESULTTEXT
+            // 
+            this.RESULTTEXT.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RESULTTEXT.Location = new System.Drawing.Point(13, 323);
+            this.RESULTTEXT.Name = "RESULTTEXT";
+            this.RESULTTEXT.Size = new System.Drawing.Size(1005, 148);
+            this.RESULTTEXT.TabIndex = 4;
+            this.RESULTTEXT.Text = "RESULTTEXT";
+            this.RESULTTEXT.TextChanged += new System.EventHandler(this.RESULTTEXT_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 532);
+            this.Controls.Add(this.RESULTTEXT);
+            this.Controls.Add(this.SQLTEXT);
+            this.Controls.Add(this.QUERYBTN);
+            this.Controls.Add(this.PNUM);
+            this.Controls.Add(this.PSTR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PNUM)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PSTR;
+        private System.Windows.Forms.NumericUpDown PNUM;
+        private System.Windows.Forms.Button QUERYBTN;
+        private System.Windows.Forms.RichTextBox SQLTEXT;
+        private System.Windows.Forms.RichTextBox RESULTTEXT;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PSTR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SQLTEXT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void QUERYBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string oradb = "Data Source="
+                    + "(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=dblab.sogang.ac.kr)(PORT=1521)))"
+                    + "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=DBU2016)));"
+                    + "User Id=db20101611;Password=unesco11;";
+
+                OracleConnection conn = new OracleConnection(oradb);
+                conn.Open();
+
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = CommandType.Text;
+
+                OracleDataReader dr;
+                int result;
+
+                int pn = (int)this.PNUM.Value;
+
+                switch (pn)
+                {
+                    case 1:
+                        cmd.CommandText =
+                            "create table branch("
+                            + "branch_name varchar(20) CONSTRAINT branch_pk PRIMARY KEY NOT NULL,"
+                            + " branch_city varchar(20),"
+                            + " assets number"
+                            + ")";
+                        this.SQLTEXT.Text = cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text = "BRANCH 테이블이 생성되었습니다";
+                        break;
+                    case 2:
+                        cmd.CommandText = "insert into branch values ('Brighton', 'Brooklyn', 7100000)";
+                        this.SQLTEXT.Text = cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text = "BRANCH 테이블에 ('Brighton', 'Brooklyn', 7100000)를 삽입했습니다.";
+
+                        cmd.CommandText = "insert into branch values ('Downtown', 'Brooklyn', 9000000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Downtown', 'Brooklyn', 9000000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('Mianus', 'Horseneck', 4000000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Mianus', 'Horseneck', 4000000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('North Town', 'Rye', 3700000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('North Town', 'Rye', 3700000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('Perryridge', 'Horesneck', 1700000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Perryridge', 'Horesneck', 1700000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('Pownal', 'Benninghton', 300000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Pownal', 'Benninghton', 300000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('Redwood', 'Palo Alto', 2100000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Redwood', 'Palo Alto', 2100000)를 삽입했습니다";
+
+                        cmd.CommandText = "insert into branch values ('Round Hill', 'Horseneck', 8000000)";
+                        this.SQLTEXT.Text += "\n" + cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text += "\n" + "BRANCH 테이블에 ('Round Hill', 'Horseneck', 8000000)를 삽입했습니다";
+
+                        break;
+
+                    case 3:
+                        cmd.CommandText = "select distinct branch_name from branch";
+                        this.SQLTEXT.Text = cmd.CommandText;
+                        dr = cmd.ExecuteReader();
+                        string coluomnName = "";
+                        for (int i = 0; i < dr.FieldCount; i++)
+                            coluomnName += "\t" + dr.GetName(i);
+                        string separatingline = "";
+                        for (int i = 0; i < dr.FieldCount; i++)
+                            separatingline += "\t" + "===========";
+                        string rowText = "";
+                        while (dr.Read())
+                        {
+                            for (int i = 0; i < dr.FieldCount; i++)
+                                rowText += "\t" + dr.GetString(i);
+                            rowText += "\n";
+                        }
+
+                        this.RESULTTEXT.Text = "\n" + coluomnName + "\n" + separatingline + "\n" + rowText;
+                        break;
+
+                    case 35:
+                        cmd.CommandText = "drop table branch";
+                        this.SQLTEXT.Text = cmd.CommandText;
+                        result = cmd.ExecuteNonQuery();
+                        this.RESULTTEXT.Text = "BRANCH 테이블을 삭제 하였습니다";
+                        break;
+                }
+
+                conn.Dispose();
+            }
+            catch (Exception ex)
+            {
+                this.RESULTTEXT.Text = ex.ToString();
+            }
+        }
+
+        private void RESULTTEXT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PNUM_ValueChanged(object sender, EventArgs e)
+        {
+            int num = (int)this.PNUM.Value;
+
+            switch (num)
+            {
+                case 1:
+                    this.PSTR.Text = "1. 다음 ER 다이어그램을 Oracle DBMS에 입력하시오 (create)";
+                    break;
+                case 2:
+                    this.PSTR.Text = "2. 다음 데이터를 입력하시오 (insert)";
+                    break;
+                case 3:
+                    this.PSTR.Text = "3. 중복되지 않은 모든 지점들의 이름을 구하라 (distinct)";
+                    break;
+                case 35:
+                    this.PSTR.Text = "35. 지금가지 생성된 View와 릴레이션을 삭제하라 (drop)";
+                    break;
+                default:
+                    this.PSTR.Text = "";
+                    break;
+            }
+        }
+    }
+}
